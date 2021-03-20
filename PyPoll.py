64a5c0b14974ad231a77a68368b35de1f793a69a
madihajaved <75510757+madihajaved@users.noreply.github.com>
2021-03-20 18:17:20 -0400

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -49,37 +49,60 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
 
         # Add a vote to that candidate's count 
-        candidate_votes[candidate_name] += 1
+        candidate_votes[candidate_name] += 1       
 
-# Determine the percentage of votes for each candidate by looping through the counts
-# 1. Iterate through the list 
-for candidate_name in candidate_votes:
-    # 2. Retrieve vote count of each candidate 
-    votes = candidate_votes[candidate_name]
-    # 3. Calculate percentage of votes 
-    vote_percentage = float(votes) / float(total_votes) * 100
+# Save the results to our text file
+with open(file_to_save, "w") as txt_file:   
+    election_results = (
+        f"\nElection Results\n"
+        f"-----------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-----------------------\n")
+    print(election_results, end="")
 
-    # Determine winning count and candidate
-    # Determine if the votes is greater than the winning count.
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
+    # Save the final count to the text file 
+    txt_file.write(election_results)           
 
-winning_candidate_summary = (
-    f"_______________________\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning vote count: {winning_count}\n"
-    f"Winning percentage: {winning_percentage:.1f}%\n"
-    f"_______________________\n")
+    # Determine the percentage of votes for each candidate by looping through the counts
+    # 1. Iterate through the list 
+    for candidate_name in candidate_votes:
+        # 2. Retrieve vote count of each candidate 
+        votes = candidate_votes[candidate_name]
+        # 3. Calculate percentage of votes 
+        vote_percentage = float(votes) / float(total_votes) * 100
 
-print(winning_candidate_summary)
+        # Print each candidate, their voter count, and percentage 
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+
+        # Save the results to our text file 
+        txt_file.write(candidate_results)
+
+        # Determine winning count and candidate
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"_______________________\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning vote count: {winning_count}\n"
+        f"Winning percentage: {winning_percentage:.1f}%\n"
+        f"_______________________\n")
+
+    print(winning_candidate_summary)
+    
+    # Save the winning candidate's name to the text file 
+    txt_file.write(winning_candidate_summary)
+
 
 # Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+#with open(file_to_save, "w") as txt_file:
 
     # Write three counties to the file.
-    txt_file.write("Arapahoe\nDenver\nJefferson")
+    #txt_file.write("Arapahoe\nDenver\nJefferson")
 
 # A complete list of candidates who received votes 
 
